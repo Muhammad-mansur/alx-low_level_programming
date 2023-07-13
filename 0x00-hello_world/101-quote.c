0f5b13795955ab9864d67f8d57a1e73f159b3f47
mansur <muhammadmansur737@gmail.com>
2023-07-13 10:49:23 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,15 @@
 #include<unistd.h>
 
 /**
-  * main - A program that prints a string to the standard error without using printf or puts
+  * main - A program that prints a string to the standard error
+  * without using printf or puts
   *
   * Return: 1 (Success)
   */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n", 59);
 	return (1);
 }
