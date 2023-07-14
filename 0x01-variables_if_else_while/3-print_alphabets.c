ca23bb556155d7ca95703ad13f1d1ebf733c7864
mansur <muhammadmansur737@gmail.com>
2023-07-14 10:48:06 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+  * main - A program that prints alphabet
+  * in lower case, and then in upper case.
+  *
+  * Return: Always zero (Success)
+  */
+
+int main(void)
+{
+	char alpha;
+
+	/* to lower case */
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	/* to upper case */
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\');
+	return (0);
+}
