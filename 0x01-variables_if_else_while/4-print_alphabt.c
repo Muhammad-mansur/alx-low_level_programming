2c361e6b8b33d47ed94c2a3044d5ec86f70a887c
mansur <muhammadmansur737@gmail.com>
2023-07-14 11:18:22 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+  * main - my entry point
+  *
+  * Description: A program that prints the alphabet
+  * in lower case without letters "q" and "e".
+  *
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++) /* to lower case */
+	{
+		if (alpha != 'q' && alpha != 'e') /* removing q and e */
+		{
+			putchar(alpha);
+		}
+
+		putchar('\n'); /* new line*/
+	}
