7b7b9082d5267208c7c49e9fa2633a827316f2b5
mansur <muhammadmansur737@gmail.com>
2023-07-15 04:09:55 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+  * main - my entry point
+  *
+  * Description: A program that prints all possible
+  * combinations of single-digit numbers.
+  *
+  * Return: 0 (success)
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+	if (a != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
+}
