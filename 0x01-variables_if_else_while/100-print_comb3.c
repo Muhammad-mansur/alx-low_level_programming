0be88a97ae1f7efc9d783b20d8f491c53e206dec
mansur <muhammadmansur737@gmail.com>
2023-07-17 10:09:58 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,30 +14,21 @@ int main(void)
 	int a;
 	int b;
 
-	a = '0';
-	b = '0';
-
-	for (a = '9'; a <= '9'; a++)
+	for (a = '0'; a <= '8'; a++)
 	{
-		for (b = '9'; b <= '9'; b++)
+		for (b = a + 1; b <= '9'; b++)
 		{
-
-			if (a < b || a == b);
+			putchar(a);
+			putchar(b);
+			if (a != '8' || b != '9')
 			{
-				putchar(a);
-				putchar(b);
-				if (a == '8' && b == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+
+	putchar('\n');
 
 	return (0);
 }
