f84c9e7dd9828a7ad0bd71794158d1d21b6ff3f9
mansur <muhammadmansur737@gmail.com>
2023-07-18 06:10:24 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - entry point
+  *
+  * Description: A function that prints 10 times
+  * the alphabet in lower case.
+  *
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int i = '0';
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (i = '0'; i < '9'; i++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+
+}
