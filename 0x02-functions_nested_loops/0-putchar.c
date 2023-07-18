6dc2e1231a0f711870e37206af98a80d2afed345
mansur <muhammadmansur737@gmail.com>
2023-07-18 01:30:54 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Descriptin: A program that prints _putchar
+  *
+  * Return: 0 (success)
+  */
+
+int main(void)
+{
+	char a = "_putchar";
+
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
