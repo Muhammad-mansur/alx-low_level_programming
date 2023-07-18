1c6c904b16e06480cb0bf0abd0247a9e9f5a12d3
mansur <muhammadmansur737@gmail.com>
2023-07-18 10:44:03 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_last_digit - entry point
+  *
+  * @n: last digit to be printed
+  *
+  * Return: 0 (success)
+  */
+
+int print_last_digit(int n)
+{
+	int digit;
+
+	digit = n % 10;
+	return (digit);
+}
