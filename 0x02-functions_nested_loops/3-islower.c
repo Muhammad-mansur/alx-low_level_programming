cf7ccac28503d2790dda6c1ea81b66e7c6101a09
mansur <muhammadmansur737@gmail.com>
2023-07-18 08:24:32 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _islower - entry point
+  *
+  *@c: The character to be checked
+  *
+  * Description: A program that checks for lowercase character
+  *
+  * Return: 0 (succes)
+  */
+
+int _islower(int c)
+{
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
