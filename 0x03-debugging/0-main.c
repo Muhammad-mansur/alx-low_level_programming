0692a247e14b3c6695498afef1da0e866bdb1d9c
mansur <muhammadmansur737@gmail.com>
2023-07-20 05:57:43 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,32 @@
+#include <main.h>
+
+/* more headers goes there */
+
+/**
+  * main - A program to print whether number
+  * stored in a variable is positive or negative
+  *
+  * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
