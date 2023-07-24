c066c1b06e27f8c8bb6e77a88d2c341ea42fbf13
mansur <muhammadmansur737@gmail.com>
2023-07-24 09:29:17 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts - function name
+  *
+  * @str: string name
+  *
+  * Return: void
+  */
+
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
