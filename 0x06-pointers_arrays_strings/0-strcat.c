edbc3f82ee3fb47802ee9fea1a0e9508f39bb04c
mansur <muhammadmansur737@gmail.com>
2023-07-26 10:03:49 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _strcat - concatenation pointer
+  *
+  * @dest: first pointer
+  * @src: second pointer
+  *
+  * Return: character
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	/* Concatenating s1 and s2 */
+	_strcat(dest, src);
+	return (dest);
+
+}
