86d669d9c18897044aff05066cdda470abe0bb28
mansur <muhammadmansur737@gmail.com>
2023-07-26 23:34:40 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,17 @@
 #include <string.h>
 
 /**
-  * _strncpy
+  * _strncpy - function name
+  *
+  * @dest: destination pointer var
+  * @src: source pointer var
+  * @n: variable
+  *
+  * Return: character
   */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	strncpy(dest, src, n);
+	return (dest);
 }
