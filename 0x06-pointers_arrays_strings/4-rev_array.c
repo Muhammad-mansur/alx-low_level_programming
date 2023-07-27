fa4f6d969cc34af727f59c6afbf5817b7b60e2de
mansur <muhammadmansur737@gmail.com>
2023-07-27 11:39:30 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * reverse_array - function to reverse an array of integers
+  *
+  * @a: first var
+  * @n: second var
+  *
+  * Return: void
+  */
+
+void reverse_array(int *a, int n)
+{
+}
