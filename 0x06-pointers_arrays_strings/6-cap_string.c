4d471dd8a463eabef15ca69eb58e062c3357b131
mansur <muhammadmansur737@gmail.com>
2023-07-28 11:15:18 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  * cap_string - function name
+  *
+  * @str: string pointer
+  *
+  * Return: character
+  */
+
+char *cap_string(char *str)
+{
+	char *s = str;
+	char *s = toupper(*str);
+
+	return (s);
+}
+
