65b23281f277db7643e56b4c3ccf91be03c27612
mansur <muhammadmansur737@gmail.com>
2023-07-29 12:42:56 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,4 +10,24 @@
 
 char *cap_string(char *str)
 {
+	int a = 0;
+	int sepa = 11;
+	char sep[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (str[a])
+	{
+		int b = 0;
+
+		while (b < sepa)
+		{
+			if ((a == 0 || str[a - 1] == sep[b]) && (str[a] >= 97 && str[a] <= 122))
+				str[a] -= 32;
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (str);
 }
