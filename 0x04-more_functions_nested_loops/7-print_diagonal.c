d7e569798fae180a9286134e4189cc50bec64064
mansur <muhammadmansur737@gmail.com>
2023-07-30 02:40:17 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * print_diagonal - function name
+  *
+  * @n: number of times character to be printed
+  *
+  * Return: nothing
+  */
+
+void print_diagonal(int n)
+{
+}
