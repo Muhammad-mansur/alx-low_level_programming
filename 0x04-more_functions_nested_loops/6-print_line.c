08ced0ed5e523f3aa02c6ffb0241515e7b91c543
mansur <muhammadmansur737@gmail.com>
2023-07-30 02:35:50 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * print_line - function name
   *
-  * @n: integer
+  * @n: number of line that gets printed
   *
   * Return: void
   */
 
 void print_line(int n)
 {
-	for (n = 0; n > 0; n++)
+	for (; n > 0; n--)
 	{
-		_putchar('_');
+		putchar('_');
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
