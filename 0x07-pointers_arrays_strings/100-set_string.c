5ae1c407e265aea2013db5167c464dffca430f2f
mansur <muhammadmansur737@gmail.com>
2023-07-31 13:39:33 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * set_string - function name
+  *
+  * @s: double pointer
+  * @to: s2
+  *
+  * Return: nothing
+  */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
