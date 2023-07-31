87f175c2d2eb8158b1b2bbf330b1d4b9d3bf58c0
mansur <muhammadmansur737@gmail.com>
2023-07-31 12:02:39 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	n = strlen(src) + 1;
-	memcpy(dest, src, n);
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
 
 	return (dest);
 }
