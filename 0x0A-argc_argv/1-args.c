d42da92897b4bbde36fd16e869a1912d53154636
mansur <muhammadmansur737@gmail.com>
2023-08-07 00:10:22 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - function name
+  *
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 1; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+
+	return (0);
+}
