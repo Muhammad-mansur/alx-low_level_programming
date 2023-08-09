dd4c6d4edf5aedbf62d1048a5ff4a855932d7ba1
mansur <muhammadmansur737@gmail.com>
2023-08-09 00:30:55 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,10 +21,9 @@ char *_strdup(char *str)
 
 	/* calculate the length of the input string */
 	for (; str[size] != '\0'; size++)
-	
-	/** Allocate memory for the duplicate string
-	 * and add 1 for the null terminator
-	 */
+
+	/* Allocate memory for the duplicate string */
+	/* and add 1 for the null terminator */
 	strr = (char *)malloc((size + 1) * sizeof(char));
 
 	if (strr == NULL)
@@ -36,7 +35,7 @@ char *_strdup(char *str)
 	  * copy the characters from the input string to the
 	  * duplicate string
 	  */
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		strr[i] = str[i];
 	}
