97a10287a055354f844a9807936ee7022369637b
mansur <muhammadmansur737@gmail.com>
2023-08-09 19:56:17 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,8 +1,46 @@
 #include "main.h"
 
 /**
+  * alloc_grid - function
+  *
+  * @width: ---
+  * @height: ---
+  *
+  * Return: 0
   */
 
 int **alloc_grid(int width, int height)
 {
+	int a, b;
+	int **p;
+
+	if (width || height <= 0)
+	{
+		return (NULL);
+	}
+
+	p = (int **)malloc(height * sizeof(int *));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		p[a] = (int *)malloc(width * sizeof(int));
+		if (p[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+			{
+				free(p[b]);
+			}
+			free(p);
+			return NULL;
+		}
+		for (b = 0; b < width; b++)
+		{
+			p[a][b] = 0;
+		}
+	}
+	return (p);
 }
