a560e150bcb3b72d1a7a3b9421c0c025687ef86e
mansur <muhammadmansur737@gmail.com>
2023-08-11 11:47:21 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	/* Memory allocation */
-	conc = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	conc = (char *)malloc((len1 + n + 1) * sizeof(char));
 
 	if (conc == NULL)
 	{
