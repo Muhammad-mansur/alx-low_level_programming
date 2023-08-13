b8b3d5e99d70a7685e444db23b400c16c82260d4
mansur <muhammadmansur737@gmail.com>
2023-08-13 12:56:34 -0700

    task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+  * is_digit - function to check if command line argument is a number
+  * or digit
+  *
+  * @p: pointer
+  *
+  * Return: 1
+  */
+
+int is_digit(char *p)
+{
+	int a;
+
+	for (a = 0; a != '\0'; a++)
+	{
+		if (p[a] < '0' || p[a] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	return (1);
+}
+
+/**
+  * main - function to multiply two positive numbers
+  *
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (is_digit(argv[1]) == 0 || is_digit(argv[2]) == 0)
+	{
+		printf("Errot\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	mul = num1 * num2;
+	return (mul);
+
+	return (0);
+}
