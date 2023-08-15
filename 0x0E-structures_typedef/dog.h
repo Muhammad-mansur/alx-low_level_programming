8f525fccddc51dba203a5cbd7d1d25c8f8ad7bdd
mansur <muhammadmansur737@gmail.com>
2023-08-15 08:57:31 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+/**
+  * struct dog - structure that contains dog list
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner name
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
