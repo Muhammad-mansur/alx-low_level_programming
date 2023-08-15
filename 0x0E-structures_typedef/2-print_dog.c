d5c9387bbdef159f39799dcc57f6e501d2842083
mansur <muhammadmansur737@gmail.com>
2023-08-15 09:42:18 -0700

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+  */
+
+void print_dog(struct dog *d)
+{
+	struct dog
+	{
+		char name;
+		int age;
+		char owner;
+	};
+
+	if (name == NULL || age == NULL || owner == NULL)
+	{
+		printf("nil");
+	}
+
+	if (d == NULL)
+		return;
+}
