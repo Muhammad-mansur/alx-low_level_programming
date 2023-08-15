fd7d2aa2fee908f889d3c38fc62daeeb03c52d71
mansur <muhammadmansur737@gmail.com>
2023-08-15 09:26:57 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+  * init_dog - dog initializer
+  * @d: structure to initialize
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  * Return: void
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef main_h
 #define main_h
 
+#include <stdio.h>
+#include <stddef.h>
+
 /**
   * struct dog - structure that contains dog list
   * @name: dog name
@@ -13,5 +16,11 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
