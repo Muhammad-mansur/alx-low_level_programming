8bef4a12c32b2a031131591dafd0a141d4425208
mansur <muhammadmansur737@gmail.com>
2023-08-15 10:47:12 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,18 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
 
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
 	}
 
 	if (d->age <= 0)
@@ -29,8 +37,6 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
 	}
 }
