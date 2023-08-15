f49d3a92e8357300546c229f8ee1e3ff0904ecb2
mansur <muhammadmansur737@gmail.com>
2023-08-15 11:34:26 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,11 @@
 #include "dog.h"
 
 /**
+  * new_dog - Function that creates a new dog
+  * @name: new dog name
+  * @age: new dog age
+  * @owner: new dog owner's name
+  * Return: new dog
   */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -8,28 +13,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* Memory allocation */
 	dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
 
-	/* Return NULL if mem allocation fails */
+	/* Return NULL if memory allocation fails */
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
 
-	/* Store a copy of name and owner */
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
-
+	/* Memory allocation for dog name */
+	dog->name = malloc(sizeof(name));
 	if (name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
+	/* Memory allocation for dog owner */
+	dog->owner = malloc(sizeof(owner));
 	if (owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
+
+	/* Store a copy of name and owner */
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
 
 	dog->age = age;
 
