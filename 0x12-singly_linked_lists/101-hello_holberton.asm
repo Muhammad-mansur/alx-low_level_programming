57fb0b44a50dbf584979c3f8fa63af4c521814ce
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-24 14:17:17 -0700

    adv 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,20 @@
 section .data
-    hello db "Hello, Holberton",10 ; The string to print with a newline character (10)
-    format db "%s",0             ; The format string for printf with a null terminator (0)
+    format db "Hello, Holberton",10,0  ; The format string for printf
 
 section .text
     global main
     extern printf
 
 main:
-    ; Push format string and the address of hello onto the stack
+    ; Push format string onto the stack
     push rdi                     ; Clear rdi to align the stack
-    push rsi                     ; Clear rsi to align the stack
     lea rdi, [format]
-    lea rsi, [hello]
 
     ; Call printf
     call printf
 
     ; Clean up the stack
-    add rsp, 16
+    add rsp, 8
 
     ; Exit the program
     mov rax, 60                  ; syscall number for exit (60)
