9983c3684daaa4c94a8b1ad99885d0986932f001
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-25 11:15:03 -0700

    adv 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,22 @@
 section .data
-    format db "Hello, Holberton",10,0  ; The format string for printf
+    hello db "Hello, Holberton", 10, 0  ; The format string for printf
 
 section .text
     global main
     extern printf
 
 main:
-    sub rsp, 8                ; Align the stack to a multiple of 16 bytes
+    ; Set up the stack frame
+    push rbp
+    mov rdi, hello          ; Load the address of the format string
+    call printf             ; Call printf
+    pop rbp
 
-    lea rdi, [format]         ; Load the address of the format string
-    call printf               ; Call printf
+    ; Exit the program
+    mov rax, 60             ; syscall number for exit (60)
+    xor rdi, rdi            ; exit status: 0
+    syscall
 
-    add rsp, 8                ; Restore the stack
-
-    mov rax, 60               ; syscall number for exit (60)
-    xor rdi, rdi              ; exit status: 0
-    syscall                   ; Invoke syscall to exit
+section .bss
+    resb 1                   ; Reserve 1 byte for a null terminator
 
