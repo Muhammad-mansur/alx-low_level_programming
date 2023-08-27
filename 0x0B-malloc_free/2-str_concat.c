4eca5923dd750a54b2db7e08d62bc9dda2a08699
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-26 23:53:29 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,10 @@ char *str_concat(char *s1, char *s2)
 
 	/* HANDLING NULL strings */
 	if (s1 == NULL)
-	{
 		s2 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	/* calculate the lengths of the strings */
 	L1 = strlen(s1);
@@ -32,21 +29,15 @@ char *str_concat(char *s1, char *s2)
 	/* Allocate memory for the concatenated string */
 	m = (char *)malloc((L1 + L2 + 1) * sizeof(char));
 	if (m == NULL)
-	{
 		return (NULL);
-	}
 
 	/* copy the content of s1 and s2 into m */
 	for (a = 0; a < L1; a++)
-	{
 		m[a] = s1[a];
-	}
 
 	a = 0;
 	for (b = 0; b < L2; b++)
-	{
 		m[a + b] = s2[b];
-	}
 
 	/* Null termination of the concatenated string */
 
