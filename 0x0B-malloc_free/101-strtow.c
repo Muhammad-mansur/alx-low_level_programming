1e74a3cd89c2debf802d63f6b8a3582c98f2249a
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-26 23:49:39 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,49 +9,49 @@
 
 char **strtow(char *str)
 {
-    char **array, *tmp;
-    int a, b = 0, len = 0, num_words, word_len = 0, start, end;
+	char **array, *tmp;
+	int a, b = 0, len = 0, num_words, word_len = 0, start, end;
 
-    while (str[len] != '\0')
-    {
-	    len++;
-    }
-    num_words = c_words(str);
-    if (num_words == 0)
-	    return (NULL);
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	num_words = c_words(str);
+	if (num_words == 0)
+		return (NULL);
 
-    array = (char **)malloc(sizeof(char *) * (num_words + 1));
-    if (array == NULL)
-	    return (NULL);
+	array = (char **)malloc(sizeof(char *) * (num_words + 1));
+	if (array == NULL)
+		return (NULL);
 
-    for (a = 0; a <= len; a++)
-    {
-	    if (str[a] == ' ' || str[a] == '\0')
-	    {
-		    if (word_len)
-		    {
-			    end = a;
-			    tmp = (char *)malloc(sizeof(char) * (word_len + 1));
-			    if (tmp == NULL)
-				    return (NULL);
-			    while (start < end)
-				    *tmp++ = str[start++];
-			    *tmp = '\0';
-			    array[b] = tmp - word_len;
-			    b++;
-			    word_len = 0;
-		    }
-	    }
-	    else if (word_len++ == 0)
-		    start = a;
-    }
-    array[b] = NULL;
-    return (array);
+	for (a = 0; a <= len; a++)
+	{
+		if (str[a] == ' ' || str[a] == '\0')
+		{
+			if (word_len)
+			{
+				end = a;
+				tmp = (char *)malloc(sizeof(char) * (word_len + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				array[b] = tmp - word_len;
+				b++;
+				word_len = 0;
+			}
+		}
+		else if (word_len++ == 0)
+			start = a;
+	}
+	array[b] = NULL;
+	return (array);
 }
 
 /**
-  * count_words - helper function to count the number of words in a string
-  * @s: the input string
+  * c_words - helper function to count the number of words in a string
+  * @st: the input string
   * Return: no of words in the string
   */
 
