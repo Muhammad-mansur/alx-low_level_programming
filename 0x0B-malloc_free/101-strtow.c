56111d6f060af244bfb79004a134902a9d7a5e27
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-26 23:41:06 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,36 +9,68 @@
 
 char **strtow(char *str)
 {
-    int num_words = 0, i = 0;
-    char **words = NULL, *token = strtok(str, " ");
+    char **array, *tmp;
+    int a, b = 0, len = 0, num_words, word_len = 0, start, end;
 
-    if (!str || !*str)
-        return (NULL);
-
-    while (token)
+    while (str[len] != '\0')
     {
-        num_words++;
-        token = strtok(NULL, " ");
+	    len++;
     }
+    num_words = c_words(str);
+    if (num_words == 0)
+	    return (NULL);
 
-    words = (char **)malloc((num_words + 1) * sizeof(char *));
-    if (!words)
-        return (NULL);
+    array = (char **)malloc(sizeof(char *) * (num_words + 1));
+    if (array == NULL)
+	    return (NULL);
 
-    token = strtok(str, " ");
-    while (token)
+    for (a = 0; a <= len; a++)
     {
-        words[i++] = strdup(token);
-        if (!words[i - 1])
-        {
-            while (--i >= 0)
-                free(words[i]);
-            free(words);
-            return (NULL);
-        }
-        token = strtok(NULL, " ");
+	    if (str[a] == ' ' || str[a] == '\0')
+	    {
+		    if (word_len)
+		    {
+			    end = a;
+			    tmp = (char *)malloc(sizeof(char) * (word_len + 1));
+			    if (tmp == NULL)
+				    return (NULL);
+			    while (start < end)
+				    *tmp++ = str[start++];
+			    *tmp = '\0';
+			    array[b] = tmp - word_len;
+			    b++;
+			    word_len = 0;
+		    }
+	    }
+	    else if (word_len++ == 0)
+		    start = a;
     }
+    array[b] = NULL;
+    return (array);
+}
 
-    words[i] = NULL;
-    return (words);
+/**
+  * count_words - helper function to count the number of words in a string
+  * @s: the input string
+  * Return: no of words in the string
+  */
+
+int c_words(char *st)
+{
+	int is_word = 0, word_count = 0, a;
+
+	for (a = 0; st[a] != '\0'; a++)
+	{
+		if (st[a] == ' ')
+		{
+			is_word = 0;
+		}
+		else if (is_word == 0)
+		{
+			is_word = 1;
+			word_count++;
+		}
+	}
+
+	return (word_count);
 }
