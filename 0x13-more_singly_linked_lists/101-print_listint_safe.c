d5f7e3d86c7db6452e8356c8948c05b85b547cd7
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-29 11:34:04 -0700

    adv 1

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,7 +17,10 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)head, head->n);
 
 		if ((head - head->next) > 0)
+		{
 			head = head->next;
+		}
+
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
