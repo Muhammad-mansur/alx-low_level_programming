ce0d3d0198baadafccb41838f5bc67be2ad49066
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-29 12:59:07 -0700

    adv

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include "function_pointers.h"
+
+/**
+  */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	unsigned char *main_ptr;
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	main_ptr = (unsigned char *)main;
+
+	for (a = 0; a <bytes; a++)
+	{
+		printf("%02x", main_ptr[a]);
+		if(a < bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include "3-calc.h"
+
+/**
+  */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[a]);
+}
