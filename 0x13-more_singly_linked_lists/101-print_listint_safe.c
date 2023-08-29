c95b0a5598cc61138d252be697334b0bae64168d
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-29 02:47:33 -0700

    adv 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,23 +8,33 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t cnt = 0, idx = 0;
+	size_t cnt = 0;
 	const listint_t *present = head;
 	const listint_t *printed[1024];
 
 	while (present != NULL)
 	{
+		size_t idx = 0;
+		int dup = 0;
+
 		for (idx = 0; idx < cnt; idx++)
 		{
 			if (printed[idx] == present)
 			{
-				printf(" [%p] %d\n", (void *)present, present->n);
-				return (cnt);
+				dup = 1;
+				break;
 			}
 		}
 
-		printed[cnt++] = present;
 		printf("[%p] %d\n", (void *)present, present->n);
+
+		if (dup)
+		{
+			printf("-> [%p] %d\n", (void *)present, present->n);
+			return (cnt);
+		}
+
+		printed[cnt++] = present;
 		present = present->next;
 	}
 
