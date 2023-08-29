c91620dce6ce03bf96d18a418eb23f3c163833f9
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-29 01:18:45 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t cnt = 0;
+	const listint_t *t = h;
 
-	while (h)
+	while (t)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", t->n);
+		t = t->next;
 		cnt++;
 	}
 
