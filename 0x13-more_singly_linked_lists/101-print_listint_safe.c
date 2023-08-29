6e67a23c28e515bac34e99ea058d38ad9d767fc7
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-29 02:29:53 -0700

    adv 2

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+  * print_listint_safe - prints linked list
+  * @head: pointer to the head of the list
+  * Return: The number of nodes in the list
+  */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t cnt = 0, idx = 0;
+	const listint_t *present = head;
+	const listint_t *printed[1024];
+
+	while (present)
+	{
+		if (present == NULL)
+		{
+			break;
+		}
+
+		for (idx = 0; idx < cnt; idx++)
+		{
+			if (printed[idx] == present)
+			{
+				printf(" [%p] %d\n", (void *)present, present->n);
+			}
+		}
+
+		printed[cnt++] = present;
+		printf("[%p] %d\n", (void *)present, present->n);
+		present = present->next;
+		cnt++;
+	}
+
+	return (cnt);
+}
