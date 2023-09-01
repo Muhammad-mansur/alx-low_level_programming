acce64bf0216cad1ba475cd78fd977220351d986
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-09-01 10:03:45 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,17 @@
-#include 
+#include "main.h"
+
+/**
+  * print_binary - prints the binary representation of a number
+  * @n: ---
+  * Return: void
+  */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	_putchar((n & 1) + '0');
+}
