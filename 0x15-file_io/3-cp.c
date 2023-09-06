4c91fda28056a354041dc47e6f0844daa608a48d
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-09-06 15:55:19 -0700

    task 3 fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,23 +35,23 @@ int main(int argc, char *argv[])
 
 	fd_s = open(file_f, O_RDONLY);
 	if (fd_s == -1)
-		print_error_exit(98, "Can't read from file");
+		print_error_exit(98, file_f);
 
 	fd_d = open(file_t, O_CREAT | O_TRUNC | O_WRONLY, 0644);
 	if (fd_d == -1)
-		print_error_exit(99, "Can't write to file");
+		print_error_exit(99, file_t);
 
 	while ((byte_r = read(fd_s, buffer, BUFFER_SIZE)) > 0)
 	{
 		byte_w = write(fd_d, buffer, byte_r);
-		if (byte_w == -1)
+		if (byte_w == -1 || byte_w != byte_r)
 		{
-			print_error_exit(99, "Can't write to file");
+			print_error_exit(99, file_t);
 		}
 	}
 
 	if (byte_r == -1)
-		print_error_exit(98, "Can't read from file");
+		print_error_exit(98, file_f);
 
 	if (close(fd_s) == -1)
 		print_error_exit(100, "Can't close fd");
