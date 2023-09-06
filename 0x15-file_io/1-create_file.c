90d53839e8b16f1e4a1c25ffdb827f1e80e9df9b
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-09-06 04:39:29 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,35 @@
 #include "main.h"
 
 /**
+  * create_file - A function that creates a file
+  * @filename: name of the file to create
+  * @text_content: NULL terminated string to write to the file
+  * Return: 1 on success or -1 on failure
   */
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd, byte_w;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		byte_w = write(fd, text_content, strlen(text_content));
+		if (byte_w == -1)
+		{
+			return (-1);
+			close(fd);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <stddef.h>
 #include <fcntl.h>
 
