a50f4d785a562025710028b0cca2bfd2f87567fd
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-09-06 10:07:21 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,18 +14,21 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes_w;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	bytes_w = write(fd, text_content, strlen(text_content));
-	if (bytes_w == -1)
+	if (text_content != NULL)
 	{
-		return (-1);
-		close(fd);
+		bytes_w = write(fd, text_content, strlen(text_content));
+		if (bytes_w == -1)
+		{
+			return (-1);
+			close(fd);
+		}
 	}
 
 	close(fd);
