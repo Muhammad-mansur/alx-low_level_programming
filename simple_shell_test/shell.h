27f62179f29e64210c644fa9e8af5c47a230919e
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-09-18 00:24:22 -0700

    test

---
+++ simple_shell_test/shell.h
@@ -0,0 +1,21 @@
+#ifndef shell_h
+#define shell_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT 1024
+
+extern char **environ;
+
+void apt();
+int read_input(char *input, size_t size);
+void tok_input(char *input, char **args);
+void exec_command(char **args);
+void handle_exit_args(char **arg);
+void _path(char *bin, char *path, char **full);
+
+#endif
